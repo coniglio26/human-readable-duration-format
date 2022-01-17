c96da00509cc836f65267c864bd062f488077d69
coniglio26 <43097613+coniglio26@users.noreply.github.com>
2022-01-17 15:00:31 +0100

    TimeUnitPresenter.java - the full toString() (for values bigger than 0 second) - presentation

--- src/main/java/hu/webstart/humanreadabledurationformat/TimeUnitPresenter.java
+++ src/main/java/hu/webstart/humanreadabledurationformat/TimeUnitPresenter.java
@@ -23,9 +23,28 @@ public class TimeUnitPresenter {
 
     @Override
     public String toString() {
+
         StringBuilder result = new StringBuilder();
         if (timeUnitAndValues.size() == 0) {
             result.append("now");
+
+        } else {
+            int j = 1;
+
+            for (Map.Entry<TimeUnit, Integer> unit : timeUnitAndValues.entrySet()) {
+
+                result.append(unit.getValue())
+                        .append(" ")
+                        .append(unit.getKey().toString().toLowerCase())
+                        .append((unit.getValue()) > 1 ? "s" : "")
+                        .append(
+                                switch (timeUnitAndValues.size() - j) {
+                                    case 0 -> "";
+                                    case 1 -> " and ";
+                                    default -> ", ";
+                                });
+                j++;
+            }
         }
         return result.toString();
     }
