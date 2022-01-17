f84c35cd748cd41724803b5e49b1dd5b79b95304
coniglio26 <43097613+coniglio26@users.noreply.github.com>
2022-01-17 14:25:28 +0100

    TimeFormatterApp.java

---
+++ src/main/java/hu/webstart/humanreadabledurationformat/TimeFormatterApp.java
@@ -0,0 +1,8 @@
+package hu.webstart.humanreadabledurationformat;
+
+public class TimeFormatterApp {
+    public static void main(String[] args) {
+        TimeFormatter timeFormatter = new TimeFormatter();
+        System.out.println(timeFormatter.formatDuration(132030240));
+    }
+}
